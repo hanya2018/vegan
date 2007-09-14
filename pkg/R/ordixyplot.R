0bf59c4b0ad6ec4ea6e1a18dcc12ae4c417e7fa3
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-14 13:55:05 +0000

    Typos.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ordixyplot.R
+++ pkg/R/ordixyplot.R
@@ -2,7 +2,7 @@
     function(x, data = NULL, formula, display = "sites", choices=1:3,
              panel = "panel.ordi", ...)
 {
-    require(lattice) || stop
+    require(lattice) || stop("requires package 'lattice'")
     x <- as.data.frame(scores(x, display = display, choices = choices))
     if (!is.null(data))
         x <- cbind(x, data)
