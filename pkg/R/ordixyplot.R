b0f6a2bbc9c45086220483770377f6af911c2068
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-16 15:28:48 +0000

    ordixyplot has iso aspect
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ordixyplot.R
+++ pkg/R/ordixyplot.R
@@ -1,10 +1,10 @@
 `ordixyplot` <-
     function(x, data = NULL, formula, display = "sites", choices=1:3,
-             panel = "panel.ordi", ...)
+             panel = "panel.ordi", aspect = "iso",  ...)
 {
     require(lattice) || stop("requires package 'lattice'")
     x <- as.data.frame(scores(x, display = display, choices = choices))
     if (!is.null(data))
         x <- cbind(x, data)
-    xyplot(formula, data = x, panel = panel,  ...)
+    xyplot(formula, data = x, panel = panel,  aspect = aspect, ...)
 }
