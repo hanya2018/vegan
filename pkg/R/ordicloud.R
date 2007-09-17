fd7c0192a64f7452b3873f62dd47c13e80bd31fd
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-17 07:12:58 +0000

    ordicloud uses now isometric aspect ratio.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ordicloud.R
+++ pkg/R/ordicloud.R
@@ -1,10 +1,11 @@
 `ordicloud` <-
     function(x, data = NULL, formula, display = "sites", choices=1:3,
-             panel = "panel.cloud", ...)
+             panel = "panel.ordi3d", prepanel = "prepanel.ordi3d",  
+             ...)
 {
     require(lattice) || stop("requires package 'lattice'")
     x <- as.data.frame(scores(x, display = display, choices = choices))
     if (!is.null(data))
         x <- cbind(x, data)
-    cloud(formula, data = x, panel = panel, ...)
+    cloud(formula, data = x, panel = panel,  prepanel = prepanel, ...)
 }
