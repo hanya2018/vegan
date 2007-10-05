41258f64ff55160eb428af591fe6094dd1df5e08
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-05 12:13:03 +0000

    ordiresids ignores now Species and Sites if these have no names (like happens with Species in capscale); kind="qqnorm" changed to "qqmath" like in lattice (and it really works with other distribution than qnorm)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@74 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ordiresids.R
+++ pkg/R/ordiresids.R
@@ -1,17 +1,20 @@
 `ordiresids` <-
-    function(x, kind = c("residuals", "scale", "qqnorm"), residuals = "working",
+    function(x, kind = c("residuals", "scale", "qqmath"), residuals = "working",
              type = c("p", "smooth", "g"), formula, ...)
 {
     require(lattice) || stop("requires package lattice")
     kind <- match.arg(kind)
-    if (!inherits(x, "cca"))
+    if (!inherits(x, "cca") || is.null(x$CCA))
         stop("function is only available for constrained ordination")
     fit <- fitted(x, type = residuals)
     res <- residuals(x, type = residuals)
     colnam <- rep(colnames(fit), each=nrow(fit))
     rownam <- rep(rownames(fit), ncol(fit))
-    df <- data.frame(Fitted = as.vector(fit), Residuals = as.vector(res),
-                     Sites=rownam, Species = colnam)
+    df <- data.frame(Fitted = as.vector(fit), Residuals = as.vector(res))
+    if (!is.null(rownam))
+        df$Sites <- rownam
+    if (!is.null(colnam))
+        df$Species <- colnam
     if (kind == "residuals") {
         if (missing(formula))
             formula <- as.formula(Residuals ~ Fitted)
@@ -22,7 +25,7 @@
             formula <- as.formula(sqrt(abs(Residuals)) ~ Fitted)
         pl <- xyplot(formula, data = df, type = type, ...)
     }
-    if (kind == "qqnorm") {
+    if (kind == "qqmath") {
         if (missing(formula))
             formula <- as.formula(~ Residuals)
         pl <- qqmath(formula, data = df, type = type, ...)
