269fe819ec5cb9f31ebf4773a67a1750bea9b15a
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-03 12:44:46 +0000

    nestedtemp handles now tied frequencies using ties.method = "average"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/nestedtemp.R
+++ pkg/R/nestedtemp.R
@@ -1,15 +1,14 @@
-"nestedtemp" <-
-function(comm, ...)
+`nestedtemp` <-
+    function(comm, ...)
 {
     comm <- ifelse(comm > 0, 1, 0)
-    i <- rev(order(rowSums(comm)))
-    j <- rev(order(colSums(comm)))
-    comm <- comm[i,j]
-    fill <- sum(comm)/prod(dim(comm))
-    r <- seq(0, 1, length=nrow(comm))
-    c <- seq(0, 1, length=ncol(comm))
+    rs <- rowSums(comm)
+    cs <- colSums(comm)
+    r <- (rank(-rs, ties="aver") - 1)/(nrow(comm)-1)
+    c <- (rank(-cs, ties="aver") - 1)/(ncol(comm)-1)
     dis <- outer(r, c, pmin)
     totdis <- 1 - abs(outer(r, c, "-"))
+    fill <- sum(comm)/prod(dim(comm))
     u <- ((dis - fill*totdis)/totdis)^2
     u[is.na(u)] <- 0
     u[dis < fill*totdis & comm == 1] <- 0
@@ -17,8 +16,9 @@ function(comm, ...)
     colnames(u) <- colnames(comm)
     rownames(u) <- rownames(comm)
     temp <- 100*sum(u)/prod(dim(comm))/0.04145
-    out <- list(comm = comm, u = u, fill=fill,  statistic = temp)
+    i <- rev(order(rs))
+    j <- rev(order(cs))
+    out <- list(comm = comm[i,j], u = u[i,j], fill=fill,  statistic = temp)
     class(out) <- "nestedtemp"
     out
 }
-
