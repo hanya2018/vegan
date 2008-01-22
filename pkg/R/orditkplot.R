8826ecc7c5c94b0e3f133bca098876814caa5e3d
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-22 13:48:58 +0000

    orditkplot points from moving labels to points and can dump the new positions of the labels in an R session
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/orditkplot.R
+++ pkg/R/orditkplot.R
@@ -30,7 +30,7 @@
     if (!is.na(idx))
         p$family <- c("helvetica", "times", "helvetica", "courier")[idx]
     saneslant <- function(x) {
-            list("roman", "bold", "italic", c("bold", "italic"))[[x]]
+        list("roman", "bold", "italic", c("bold", "italic"))[[x]]
     }
     fnt <- c(p$family, round(p$ps*p$cex*cex), saneslant(p$font))
     fnt.axis <- c(p$family, round(p$ps*p$cex.axis), saneslant(p$font.axis))
@@ -43,13 +43,44 @@
     XSCR <- as.numeric(tkwinfo("screenwidth", w))
     ## Buttons
     buts <- tkframe(w)
-    tkpack(buts, side="bottom", fill="x", pady="2m")
     cp2eps <- tkbutton(buts, text="Copy to EPS", 
-                       command=function(...) tkpostscript(can, x=0, y=0,
+                       command=function() tkpostscript(can, x=0, y=0,
                        height=height, width=width, 
                        file=tkgetSaveFile(defaultextension=".eps")))
     dismiss <- tkbutton(buts, text="Dismiss", command=function() tkdestroy(w))
-
+    ## Button to dump new label locations to an R object
+    pDump <- function() {
+        xy <- matrix(0, nrow=nrow(sco), ncol=2)
+        rownames(xy) <- rownames(sco)
+        colnames(xy) <- colnames(sco)
+        for(nm in names(pola)) {
+            xy[tclvalue(labtext[[nm]]),] <- xy2usr(nm)
+        }
+        dumpVar <- tclVar("")
+        tt <- tktoplevel()
+        tktitle(tt) <- "R Dump"
+        entryDump <- tkentry(tt, width=20, textvariable=dumpVar)
+        tkgrid(tklabel(tt, text="Enter name for an R object"))
+        tkgrid(entryDump)
+        isDone <- function() {
+            dumpName <- tclvalue(dumpVar)
+            if (exists(dumpName, envir=.GlobalEnv)) {
+                ok <- tkmessageBox(message=paste(dumpName, "exists.\nOK to overwrite?"),
+                                   icon="warning", type="okcancel", default="ok")
+                if(tclvalue(ok) == "ok") {
+                    assign(dumpName, xy, envir=.GlobalEnv)
+                    tkdestroy(tt)
+                }
+            }
+            else {
+                assign(dumpName, xy, envir=.GlobalEnv)
+                tkdestroy(tt)
+            }
+        }
+        tkbind(entryDump, "<Return>", isDone)
+        tkfocus(tt)
+    }
+    dump <- tkbutton(buts, text="Dump to R", command=pDump)
     ## Make canvas
     sco <- scores(x, display=display, ...)
     labs <- rownames(sco)
@@ -70,7 +101,7 @@
     ## Canvas like they were in the default devices when I last checked
     if (missing(width)) {
         if (options("device") == "quartz")
-            width <- 5.99
+            width <- 5
         else
             width <- 6.99
     }
@@ -83,10 +114,18 @@
     xincr <- xusr/diff(xrange)
     yincr <- xincr
     xy0 <- c(xrange[1], yrange[2]) # upper left corner
-    ## Function to translate scores to canvas coordinates
+    ## Functions to translate scores to canvas coordinates and back
     usr2xy <- function(row) {
         x <- round((row[1] - xy0[1]) * xincr) + mar[2]
         y <- round((xy0[2] - row[2]) * yincr) + mar[3]
+        c(x,y)
+    }
+    xy2usr <- function(item) {
+        xy <- as.numeric(tkcoords(can, item))
+        x <- xy[1] 
+        y <- xy[2] 
+        x <- xrange[1] + (x - mar[2])/xincr 
+        y <- yrange[2] - (y - mar[3])/yincr 
         c(x,y)
     }
     ## Equal aspect ratio
@@ -100,9 +139,10 @@
     yscr <- tkscrollbar(w, command = function(...) tkyview(can, ...))
     tkconfigure(can, yscrollcommand = function(...) tkset(yscr, ...))
     ## Pack it up
+    tkpack(buts, side="bottom", fill="x", pady="2m")
     tkpack(can, side="left", fill="x")
     tkpack(yscr, side="right", fill="y")
-    tkgrid(cp2eps, dismiss, sticky="s")
+    tkgrid(cp2eps, dump, dismiss, sticky="s")
     
     ## Box
     x0 <- usr2xy(c(xrange[1], yrange[1]))
@@ -140,6 +180,8 @@
     }
     ## Points and labels
     laboff <- round(p$ps/2 + diam + 1)
+    pola <- tclArray()
+    labtext <- tclArray()
     for (i in 1:nrow(sco)) {
         xy <- usr2xy(sco[i,])
         item <- tkcreate(can, "oval", xy[1]-diam, xy[2]-diam,
@@ -149,6 +191,8 @@
                         fill = p$col, font=fnt)
         tkaddtag(can, "point", "withtag", item)
         tkaddtag(can, "label", "withtag", lab)
+        pola[[lab]] <- item
+        labtext[[lab]] <- labs[i]
     }
     ## Plotting and Moving
     pDown <- function(x, y) {
@@ -157,6 +201,10 @@
         tkdtag(can, "selected")
         tkaddtag(can, "selected", "withtag", "current")
         tkitemraise(can, "current")
+        p <- as.numeric(tkcoords(can,
+                                 pola[[tkfind(can, "withtag", "current")]]))
+        .pX <<- (p[1]+p[3])/2
+        .pY <<- (p[2]+p[4])/2
         .lastX <<- x
         .lastY <<- y
     }
@@ -164,18 +212,28 @@
         x <- as.numeric(x)
         y <- as.numeric(y)
         tkmove(can, "selected", x - .lastX, y - .lastY)
+        tkdelete(can, "ptr")
         .lastX <<- x
         .lastY <<- y
+        xadj <- as.numeric(tkcanvasx(can, 0))
+        yadj <- as.numeric(tkcanvasy(can, 0))
+        conn <- tkcreate(can, "line", .lastX + xadj, .lastY+yadj,
+                         .pX, .pY, fill="red")
+        tkaddtag(can, "ptr", "withtag", conn)
     }
     ## Dummy location of the mouse
     .lastX <- 0
-    .lastY <- 0 
+    .lastY <- 0
+    .pX <- 0
+    .pY <- 0
     ## Highlight a label when mouse moves in
     tkitembind(can, "label", "<Any-Enter>",
                function() tkitemconfigure(can, "current", fill="red"))
     tkitembind(can, "label", "<Any-Leave>",
-               function() tkitemconfigure(can, "current", fill="black"))
+               function() tkitemconfigure(can, "current", fill=p$col))
     tkitembind(can, "label", "<1>", pDown)
-    tkitembind(can, "<ButtonRelease-1>", function(x) tkdtag(can, "selected")) 
+    tkitembind(can, "label", "<ButtonRelease-1>",
+               function() {tkdtag(can, "selected"); tkdelete(can, "ptr")})
+    
     tkbind(can, "<B1-Motion>", pMove)
 }
