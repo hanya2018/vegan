eb67a4d2d896e447abae999545406f801d625d83
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-30 20:13:29 +0000

    Forgot to commit plot.orditkplot...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@178 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.orditkplot.R
+++ pkg/R/plot.orditkplot.R
@@ -4,7 +4,7 @@
     op <- par(x$par)
     on.exit(par(op))
     plot(x$points, pch = 21, cex = x$args$pcex, col = x$args$col,
-         bg = x$args$bg, asp=1)
+         bg = x$args$bg, xlim = x$args$xlim, ylim = x$args$ylim, asp=1)
     text(x$labels, rownames(x$labels), cex = x$args$cex)
     invisible(x)
 }
