8a3db0dbbb2d5160b7d5d2ac0852c2e0c3dcc338
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-02 15:30:47 +0000

    Internal functions embedded in CCorA
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CCorA.R
+++ pkg/R/CCorA.R
@@ -3,6 +3,59 @@
 {
     require(MASS) || stop("requires packages 'MASS'")
     epsilon <- sqrt(.Machine$double.eps)
+    ## BEGIN: Internal functions
+    cov.inv <- function(mat, no, epsilon) {
+        ## This function returns:
+        ## 1) mat = the matrix of PCA object scores (by SVD);
+        ## 2) S.inv = the inverse of the covariance matrix;
+        ## 3) m = the rank of matrix 'mat'
+        ## The inverse of the PCA covariance matrix is simply the
+        ## diagonal matrix of (1/eigenvalues).  If ncol(mat) = 1, the
+        ## inverse of the covariance matrix simply contains
+        ## 1/var(mat).
+        mat <- as.matrix(mat)
+        if(ncol(mat) == 1) {
+            S.inv <- as.matrix(1/var(mat))
+            m <- 1
+        } else {
+            S.svd <- svd(cov(mat))
+            m <- ncol(mat)
+            mm <- length(which(S.svd$d > epsilon))
+            if(mm < m) {
+                message("Information - Matrix",no,": rank=",mm," < order",m)
+                m <- mm
+            }
+            S.inv <- diag(1/S.svd$d[1:m])
+            mat <- mat %*% S.svd$u[,1:m]
+        }
+        list(mat=mat, S.inv=S.inv, m=m)
+    }
+    ## Check zero variances
+    var.null <- function (mat, no) {
+        problems <- diag(cov(mat)) <= 0
+        if (any(problems)) {
+            whichProbs <- paste(which(problems), collapse=", ")
+            warning("zero variance in variable(s) ", whichProbs)
+            stop("verify/modify your matrix No. ", no)
+        }
+        invisible(0)
+    }
+    probPillai <- function(Y, X, n, S11.inv, S22.inv, s, df1, df2, epsilon,
+                           Fref, nperm, ...) {
+        ## Permutation test for Pillai's trace in CCorA.
+        ## Reference: Brian McArdle's unpublished graduate course notes.
+        nGE <- 1
+        for(i in 1:nperm) {
+            Y.per <- Y[permuted.index(n, ...),, drop=FALSE]
+            S12.per <- cov(Y.per,X)
+            gross.mat <- S12.per %*% S22.inv %*% t(S12.per) %*% S11.inv
+            Pillai.per <- sum(diag(gross.mat))
+            Fper  <- (Pillai.per*df2)/((s-Pillai.per)*df1)
+            if(Fper >= (Fref-epsilon)) nGE <- nGE+1
+        }
+        P <- nGE/(nperm+1)
+    }
+    ## END: internal functions
     Y <- as.matrix(Y)
     var.null(Y,1)
     nY <- nrow(Y)
