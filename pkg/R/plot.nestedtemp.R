17dd372a41187d697bd26d5bc3cc36f207cfac73
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-11 16:40:50 +0000

    Smooth fill line in nestedtemp -- but needs checking (may be wrong)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@199 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.nestedtemp.R
+++ pkg/R/plot.nestedtemp.R
@@ -1,14 +1,13 @@
-"plot.nestedtemp" <-
-function(x, kind = c("temperature", "incidendce"), col=rev(heat.colors(100)),  ...)
+`plot.nestedtemp` <-
+    function (x, kind = c("temperature", "incidendce"), col = rev(heat.colors(100)), 
+              ...) 
 {
     kind <- match.arg(kind)
-    if (kind == "temperature")
+    if (kind == "temperature") 
         z <- x$u
-    else
-        z <- x$comm
-    z <- t(z[nrow(z):1,])
-    image(z, axes=FALSE, col = col,  ...)
+    else z <- x$comm
+    z <- t(z[nrow(z):1, ])
+    image(z, axes = FALSE, col = col, ...)
     box()
-    lines(c(0, x$fill, 1), c(0, 1-x$fill, 1))
+    lines(x$smooth$x, 1-x$smooth$y)
 }
-
