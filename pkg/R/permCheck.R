bcf39670e7b71e17d14372e2f2df1784427128df
gsimpson <gsimpson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-03-29 23:54:09 +0000

    Improvements to permCheck; more sanity checks for designs and better handling of numeric or integer vectors of length 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@295 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/permCheck.R
+++ pkg/R/permCheck.R
@@ -1,5 +1,28 @@
 `permCheck` <- function(object, control = permControl())
 {
+    ## if object is numeric or integer and of length 1,
+    ## extend the object
+    if(length(object) == 1 &&
+       (is.integer(object) || is.numeric(object)))
+        object <- seq_len(object)
+    ## check the number of observations in object
+    nobs <- getNumObs(object)
+    ## if strata, check nobs == length of strata
+    ## but beware empty levels
+    if(!is.null(control$strata)) {
+        tab <- table(control$strata)
+        if(!identical(as.integer(nobs), as.integer(sum(tab))))
+            stop("Number of observations and length of 'strata' do not match.")
+        ## if "grid", check design balanced?
+        if((bal <- length(unique(tab))) > 1 && control$type == "grid")
+            stop("Unbalanced 'grid' designs are not supported.")
+        ## if constant, check design balanced?
+        if(control$constant && bal > 1)
+            stop("Unbalanced designs not allowed with 'constant = TRUE'.")
+        ## if permuting strata, must be balanced
+        if(control$type == "strata" && bal > 1)
+            stop("Design must be balanced if permuting 'strata'.")
+    }
     ## get number of possible permutations
     num.pos <- numPerms(object, control)
     if(num.pos < control$minperm) {
