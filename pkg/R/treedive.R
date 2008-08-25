c26d2e2971221231781964e0f2c9b080f01c604e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-08-25 16:39:58 +0000

    treedive tries to match 'comm' and 'tree', docs tell to use oecosimu for testing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@485 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/treedive.R
+++ pkg/R/treedive.R
@@ -1,9 +1,21 @@
 `treedive` <-
-    function(comm, tree)
+    function(comm, tree, match.force = FALSE)
 {
     if (!inherits(tree, "hclust"))
         stop("'clus' must be an 'hclust' result object")
     m <- as.matrix(cophenetic(tree))
+    ## Check tree/comm match by names
+    if (match.force || ncol(comm) != length(tree$order)) {
+        fnd <- colnames(comm) %in% tree$labels
+        if (!all(fnd)) {
+            warning("not all names of 'comm' found in 'tree'")
+            comm <- comm[, fnd]
+        }
+        fnd <- tree$labels %in% colnames(comm)
+        if (!all(fnd))
+            warning("not all names of 'tree' found in 'comm'")
+        comm <- comm[, tree$labels[fnd]]
+    }
     ## Repeat for sites
     div <- numeric(nrow(comm))
     for (i in 1:nrow(comm)) {
@@ -12,6 +24,7 @@
         cl <- update(tree, d = d)
         div[i] <- treeheight(cl)
     }
+    names(div) <- rownames(comm)
     div
 }
 
