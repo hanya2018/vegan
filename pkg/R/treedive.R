fea3aa70937f0567da5b5edd0d1e8808994acf26
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-26 10:54:06 +0000

    treedive: improved matching of tree and comm names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@506 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/treedive.R
+++ pkg/R/treedive.R
@@ -6,6 +6,10 @@
     m <- as.matrix(cophenetic(tree))
     ## Check tree/comm match by names
     if (match.force || ncol(comm) != length(tree$order)) {
+        if (match.force)
+            message("Forced matching of 'tree' labels and 'comm' names")
+        else
+            message("Dimensions do not match between 'comm' and 'tree'")
         fnd <- colnames(comm) %in% tree$labels
         if (!all(fnd)) {
             warning("not all names of 'comm' found in 'tree'")
@@ -15,6 +19,10 @@
         if (!all(fnd))
             warning("not all names of 'tree' found in 'comm'")
         comm <- comm[, tree$labels[fnd]]
+        if (length(unique(tree$labels)) != length(tree$labels))
+            stop("names not unique in 'tree': match wrong")
+        if (length(unique(colnames(comm))) != ncol(comm))
+            stop("names not unique in 'comm': match wrong")
     }
     ## Repeat for sites
     div <- numeric(nrow(comm))
@@ -27,4 +35,3 @@
     names(div) <- rownames(comm)
     div
 }
-
