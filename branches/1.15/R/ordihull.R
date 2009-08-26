ee94dd31a9678371f1d50df3b5d632c108cc4a98
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-26 15:58:07 +0000

    merged summary for ordihull & ordiellipse into branches/115
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vegan@957 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/1.15/R/ordihull.R
+++ branches/1.15/R/ordihull.R
@@ -1,5 +1,6 @@
 "ordihull" <-
-    function (ord, groups, display = "sites", draw = c("lines", "polygon"),
+    function (ord, groups, display = "sites",
+              draw = c("lines", "polygon", "none"),
               show.groups, label = FALSE, ...)
 {
     draw <- match.arg(draw)
@@ -20,9 +21,10 @@
             hpts <- c(hpts, hpts[1])
             if (draw == "lines")
                 ordiArgAbsorber(X[hpts, ], FUN = lines, ...)
-            else ordiArgAbsorber(X[hpts,], FUN = polygon, ...)
-            if (label) {
-                cntr <- colMeans(X[hpts[-1],])
+            else if (draw == "polygon")
+                ordiArgAbsorber(X[hpts,], FUN = polygon, ...)
+            if (label && draw != "none") {
+                cntr <- colMeans(X)
                 ordiArgAbsorber(cntr[1], cntr[2], labels = is,
                                 FUN = text, ...)
             }
